1f6b829a858d891a0aacc2480e522935877a7c6a
Shivam Pasvan <shivampasvan@gmail.com>
2024-02-07 19:15:52 +0530

    recheck

--- src/components/Webinar.jsx
+++ src/components/Webinar.jsx
@@ -17,9 +17,12 @@ import {
   ModalBody,
   ModalCloseButton,
   useToast,
+
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import url from "../data/liveworkshop.mp4";
+import moburl from "../data/liveworkshopformobile.mp4";
+
 import { AiFillCalendar } from "react-icons/ai";
 import { FaRegClock } from "react-icons/fa";
 
@@ -56,23 +59,25 @@ const Webinar = () => {
         isClosable: true,
       });
     }
-    // onClose();
   };
 
   return (
     <>
-      <Box>
+{/* for mobile */}
+
+      <Box display={{base:'block', sm:'none', md:'none', lg:'none'}}>
+        <Box>
         <video autoPlay loop playsInline muted width={"auto"} height={"70%"}>
-          <source src={url} type="video/mp4" />
+          <source src={moburl} type="video/mp4" />
         </video>
-      </Box>
-      <Grid
-        width={"36%"}
-        marginLeft={"10%"}
-        position={"absolute"}
-        bottom={{ base: "5%", sm: "8%", md: "8%", lg: "6%" }}
-        gap={{ base: 2, sm: 3, md: 4, lg: 10 }}
+        </Box>
+        <Grid
+        width={{base:"95%",lg:"36%"}}
+        margin={"3%"}
+        bottom={{ base: "0%", sm: "8%", md: "8%", lg: "6%" }}
+        gap={{ base: 1, sm: 3, md: 4, lg: 10 }}
       >
+        <Grid>
         <Modal
           closeOnOverlayClick={false}
           isOpen={isOpen}
@@ -133,10 +138,12 @@ const Webinar = () => {
             </ModalFooter>
           </ModalContent>
         </Modal>
+        </Grid>   
 
+        <Grid gap={4} mt={'-24px'}>  
         <Box alignContent={"start"}>
           <Text
-            fontSize={{ base: "10px", sm: "12px", md: "14px", lg: "22px" }}
+            fontSize={{ base: "15px", sm: "12px", md: "14px", lg: "22px" }}
             fontWeight={"600"}
           >
             Join our free, exclusive webinar by Stocktutor's 20-year experienced
@@ -144,7 +151,130 @@ const Webinar = () => {
             understanding of the stock market
           </Text>
         </Box>
-        <Flex fontSize={18} fontWeight={600} gap={16}>
+        <Box>
+        <Flex fontSize={{base:14, lg:18}} fontWeight={600} gap={{base:8 ,lg:16}}>
+          <Flex alignItems={"center"} gap={2}>
+            <AiFillCalendar />
+            <Text>13 Feb 2024</Text>
+          </Flex>
+
+          <Flex alignItems={"center"} gap={2}>
+            <FaRegClock />
+            <Text>2 PM</Text>
+          </Flex>
+        </Flex>
+        </Box>
+        <Box>
+        <Flex>
+          <Button
+            size={{ base: "sm", sm: "sm", md: "sm", lg: "lg" }}
+            // fontSize={{ base: "10px", sm: "sm", md: "sm", lg: "lg" }}
+            bg={"rgb(5,8,69)"}
+            color={"white"}
+            onClick={onOpen}
+            _hover={{
+              border:"1px solid silver",
+              color:"rgb(5,8,69)",
+              background:"silver"
+            }}
+          >
+            Register Now
+          </Button>
+        </Flex>
+        </Box>
+        </Grid>  
+      </Grid>
+      </Box>
+
+
+{/* for desktop */}
+      <Box display={{base:'none', sm:'none', md:'block', lg:'block'}}>
+      <Box>
+        <video autoPlay loop playsInline muted width={"auto"} height={"70%"}>
+          <source src={url} type="video/mp4" />
+        </video>
+      </Box>
+      <Grid
+        width={{base:"48%",lg:"36%"}}
+        marginLeft={"10%"}
+        position={"absolute"}
+        bottom={{ base: "2%", sm: "8%", md: "8%", lg: "6%" }}
+        gap={{ base: 1, sm: 3, md: 4, lg: 10 }}
+        // border={'2px solid red'}
+      >
+        <Grid width={{base:"70%"}} >
+        <Modal
+          closeOnOverlayClick={false}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Please fill the details !!</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6} margin={4}>
+              <SimpleGrid gap={6}>
+                <FormControl id="name" isRequired>
+                  <FormLabel>Full Name</FormLabel>
+                  <Input
+                    value={name}
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </FormControl>
+
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    value={email}
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormControl>
+
+                <FormControl id="number" isRequired>
+                  <FormLabel>Mobile Number</FormLabel>
+                  <Input
+                    value={number}
+                    type="number"
+                    onChange={(e) => setNumber(e.target.value)}
+                  />
+                </FormControl>
+              </SimpleGrid>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                loadingText="Submitting"
+                colorScheme="blue"
+                mr={3}
+                onClick={handleClick}
+                bg={"rgb(5,8,69)"}
+                size={"md"}
+                _hover={{
+                  border:"1px solid silver",
+                  color:"rgb(5,8,69)",
+                  background:"silver"
+                }}
+              >
+                Submit
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        </Grid>       
+        <Box alignContent={"start"} width={'100%'}>
+          <Text
+            fontSize={{ base: "8px", sm: "12px", md: "14px", lg: "22px" }}
+            fontWeight={"600"}
+          >
+            Join our free, exclusive webinar by Stocktutor's 20-year experienced
+            mentors. Learn creative ways to grow your money and gain a deep
+            understanding of the stock market
+          </Text>
+        </Box>
+        <Flex fontSize={{base:8, lg:18}} fontWeight={600} gap={{base:4 ,lg:16}}>
           <Flex alignItems={"center"} gap={2}>
             <AiFillCalendar />
             <Text>13 Feb 2024</Text>
@@ -158,6 +288,7 @@ const Webinar = () => {
         <Flex>
           <Button
             size={{ base: "xs", sm: "sm", md: "sm", lg: "lg" }}
+            // fontSize={{ base: "10px", sm: "sm", md: "sm", lg: "lg" }}
             bg={"rgb(5,8,69)"}
             color={"white"}
             onClick={onOpen}
@@ -171,6 +302,7 @@ const Webinar = () => {
           </Button>
         </Flex>
       </Grid>
+      </Box>
     </>
   );
 };
